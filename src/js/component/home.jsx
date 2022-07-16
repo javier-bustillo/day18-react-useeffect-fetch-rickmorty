3a6042cc6409fd16e568e4a5515c84339d4368f9
javier-bustillo <jjavier.bustillo@gmail.com>
2022-07-16 10:43:58 +0000

    initial commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [characters, setCharacters] = useState([]);
+
+	useEffect{()=>{
+		fetch("https://rickandmortyapi.com/api/character")
+			.then((resp) => resp.json())
+			.then((data) => setCharacters(data.results));
+	}, []};
+
 	return (
 		<div className="text-center">
 			<h1 className="text-center mt-5">Hello Rigo!</h1>
